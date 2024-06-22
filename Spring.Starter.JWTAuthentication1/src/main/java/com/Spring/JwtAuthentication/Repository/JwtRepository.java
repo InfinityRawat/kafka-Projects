6c2a90e0a7471132cfee8a203d9ff276c623cbf0
InfinityRawat <124661167+InfinityRawat@users.noreply.github.com>
2024-06-22 23:01:22 +0530

    Delete Spring.Starter.JWTAuthentication1 directory

--- Spring.Starter.JWTAuthentication1/src/main/java/com/Spring/JwtAuthentication/Repository/JwtRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Spring.JwtAuthentication.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Spring.JwtAuthentication.Entity.UserEntity;
-
-public interface JwtRepository extends JpaRepository<UserEntity,String> {
-
-}
